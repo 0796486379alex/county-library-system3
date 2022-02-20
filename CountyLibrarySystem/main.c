510ffa19af9e2050f7988ab472016f555fa87011
0796486379alex <98796291+0796486379alex@users.noreply.github.com>
2022-02-20 17:55:57 +0300

    Add files via upload

---
+++ CountyLibrarySystem/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+printf("\tCounty Library\n");
+printf("Welcome to Mr. Alex.\n");
+printf("what would like to do?\n");
+printf("1.Mr Kevin\n");
+printf("2.Waterfall model\n");
+printf("Welcome to Mr Alex.\n");
+    return 0;
+}
